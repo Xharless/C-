1e4544de63557f5a4108d9054991f9b2e6c8c9ba
Xharless <carlosarevalo1702@hotmail.com>
2024-08-06 12:00:26 -0400

    introduccion c++

---
+++ primer_programa.cpp
@@ -0,0 +1,8 @@
+// siempre hay que agregar las librerias o sino no funcional
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<<"hola mundo"<<endl; // el endl da un salto al final  
+    return 0; //para que termine
+}
---
+++ tipos_de_datos.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int numero = 15; //variable entera que es un espacio de memoria en donde guardo algo determinado, en este caso en la direccion de memoria tengo 15
+    float flotante = 10.45;
+    double mayor = 16.3456; // el double es como el flotante pero puedes guardar numeros mas extensos 
+    char letra = 'a'; //con comillas simples pero son caracteres
+    cout<<numero<<endl; 
+    cout<<flotante<<endl; 
+    cout<<mayor<<endl;
+    cout<<letra<<endl;
+
+    return 0;
+}
