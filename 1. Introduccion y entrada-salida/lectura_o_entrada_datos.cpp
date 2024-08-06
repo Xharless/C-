791445ad72f8c2269a141d2dbc4ff43b6d11a8df
Xharless <carlosarevalo1702@hotmail.com>
2024-08-06 12:08:23 -0400

    entradas y salidas

---
+++ 1. Introduccion y entrada-salida/lectura_o_entrada_datos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int numero; // si o si tiene que ser entero, si quisiera ingresar float tengo que poner float numero
+    cout<<"digite un numero:";
+    cin>>numero; // guarda el resultado del cout en la variable del numero 
+    cout<<"\nEl numero es: " << numero; //muestra el numero luego
+
+    return 0;
+}
